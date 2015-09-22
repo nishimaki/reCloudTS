96b6f4809dc20d065d690bfd3beee81f1168537a
Toshi <Toshi@MacBook-Pro.local>
2015-09-22 23:09:21 +0900

    新規作成

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name reCloudTsApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the reCloudTsApp
+ */
+angular.module('reCloudTsApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name reCloudTsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the reCloudTsApp
+ */
+angular.module('reCloudTsApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
